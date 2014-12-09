be4450075e9211f5e1dc62db9a3305944aba953f
AsafMah <mahlev.asaf@gmail.com>
2014-12-09 18:18:09 +0200

    Initial commit! Probably should have done it sooner

---
+++ RebirthManager/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace RebirthManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RebirthManagerLabel = new System.Windows.Forms.Label();
+            this.ReadLastRun = new System.Windows.Forms.Button();
+            this.UpdateItemTables = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RebirthManagerLabel
+            // 
+            this.RebirthManagerLabel.AutoSize = true;
+            this.RebirthManagerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.RebirthManagerLabel.Location = new System.Drawing.Point(136, 19);
+            this.RebirthManagerLabel.Name = "RebirthManagerLabel";
+            this.RebirthManagerLabel.Size = new System.Drawing.Size(296, 42);
+            this.RebirthManagerLabel.TabIndex = 0;
+            this.RebirthManagerLabel.Text = "Rebirth Manager";
+            // 
+            // ReadLastRun
+            // 
+            this.ReadLastRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.ReadLastRun.Location = new System.Drawing.Point(12, 19);
+            this.ReadLastRun.Name = "ReadLastRun";
+            this.ReadLastRun.Size = new System.Drawing.Size(95, 55);
+            this.ReadLastRun.TabIndex = 1;
+            this.ReadLastRun.Text = "Read Last Run";
+            this.ReadLastRun.UseVisualStyleBackColor = true;
+            this.ReadLastRun.Click += new System.EventHandler(this.ReadLastRun_Click);
+            // 
+            // UpdateItemTables
+            // 
+            this.UpdateItemTables.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.UpdateItemTables.Location = new System.Drawing.Point(438, 19);
+            this.UpdateItemTables.Name = "UpdateItemTables";
+            this.UpdateItemTables.Size = new System.Drawing.Size(143, 55);
+            this.UpdateItemTables.TabIndex = 2;
+            this.UpdateItemTables.Text = "Update Database ";
+            this.UpdateItemTables.UseVisualStyleBackColor = true;
+            this.UpdateItemTables.Click += new System.EventHandler(this.UpdateItemTables_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 761);
+            this.Controls.Add(this.UpdateItemTables);
+            this.Controls.Add(this.ReadLastRun);
+            this.Controls.Add(this.RebirthManagerLabel);
+            this.Name = "MainForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Rebirth Manager";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RebirthManagerLabel;
+        private System.Windows.Forms.Button ReadLastRun;
+        private System.Windows.Forms.Button UpdateItemTables;
+    }
+}
+
---
+++ RebirthManager/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RebirthManager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public static byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReadLastRun_Click(object sender, EventArgs e)
+        {
+        
+            if(Properties.Settings.Default.LogFileLocation == "" || !File.Exists(Properties.Settings.Default.LogFileLocation)) //check if there is a a log file, if not go and find it
+            {
+                if(File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + Properties.Resources.LogFileDefault))
+                {
+                    Properties.Settings.Default.LogFileLocation = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + Properties.Resources.LogFileDefault;
+                    MessageBox.Show("Log file automatically found!");
+                    ProcessLogFile();
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                ProcessLogFile();
+            }
+        }
+
+        private void ProcessLogFile()
+        {
+            string lines;
+            using (StreamReader sr = new StreamReader(Properties.Settings.Default.LogFileLocation))
+            {
+                lines = sr.ReadToEnd();
+            }
+            string seed = lines.Substring(lines.IndexOf("RNG Start Seed: ")+16, 9);
+            
+            testlabel.Text = "Seed: "+ seed ;
+        }
+       
+        private void UpdateItemTables_Click(object sender, EventArgs e)
+        {
+            //Update Items
+
+            var ItemImageList = Directory.GetFiles("Items");
+            string[] Items = Properties.Resources.ItemList.Split('\n');
+            int counter = 0;
+            foreach (string item in Items)
+            {
+                string[] prop = item.Split(':');
+                string ID = prop[0], Name = prop[1].Split('-')[0], Desc = prop[1].Split('-')[1];
+                //Formating the item name
+                if (Name[0] == '*')
+                {
+                    Name = Name.Substring(1);
+                }
+                if (Name.Contains('['))
+                {
+                    Name = Name.Substring(0, Name.IndexOf('['));
+                }
+                if(Name.Contains(", The"))
+                {
+                    Name = "The " + Name.Substring(0, Name.IndexOf(','));
+                }
+                if(Name.Contains(", A"))
+                {
+                    Name = "A " + Name.Substring(0, Name.IndexOf(','));
+                }
+                
+                using (var ta = new IsaacDataSetTableAdapters.ItemsTableAdapter())
+                {
+
+                    try
+                    {
+                        ta.Insert(int.Parse(ID), Name, Desc, imageToByteArray(Image.FromFile(ItemImageList[counter])));
+                    }
+                    catch (Exception ex)
+                    {
+                        //No need to update the item, already exists
+                    }
+                }
+                counter++;
+            }
+
+            //Update Characters
+            var CharacterImageList = Directory.GetFiles("Characters");
+            foreach (var character in CharacterImageList)
+            {
+                string filename = Path.GetFileName(character);
+                string ID = filename.Split('-')[0], Name = filename.Split('-')[1].Split('.')[0];
+                using (var ta = new IsaacDataSetTableAdapters.CharactersTableAdapter())
+                {
+
+                    try
+                    {
+                        ta.Insert(int.Parse(ID), Name, imageToByteArray(Image.FromFile(character)));
+                    }
+                    catch (Exception ex)
+                    {
+                        //No need to update the item, already exists
+                    }
+                }
+            }
+
+        }
+    }
+}
